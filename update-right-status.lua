4134519950ff197f3c5e0980182310510f452b83
Abhishek <abhishek@Jatins-MacBook-Air.local>
2023-11-17 20:28:52 +0530

    fix: update right status according to new version

--- update-right-status.lua
+++ update-right-status.lua
@@ -22,26 +22,33 @@ function M.init(wezterm, terminal)
 
 		local cwd_uri = pane:get_current_working_dir()
 		if cwd_uri then
-			cwd_uri = cwd_uri:sub(8)
-			local slash = cwd_uri:find("/")
 			local cwd = ""
 			local hostname = ""
-			if slash then
-				hostname = cwd_uri:sub(1, slash - 1)
-				-- Remove the domain name portion of the hostname
-				local dot = hostname:find("[.]")
-				if dot then
-					hostname = hostname:sub(1, dot - 1)
-				end
-				-- and extract the cwd from the uri
-				cwd = cwd_uri:sub(slash)
-				-- table.insert(cells, cwd)
-				if hostname == "" then
-					table.insert(cells, "")
-				elseif string.find(hostname, "arch") then
-					table.insert(cells, "")
-				else
-					table.insert(cells, "")
+			if type(cwd_uri) == "userdata" then
+				-- Running on a newer version of wezterm and we have
+				-- a URL object here, making this simple!
+				cwd = cwd_uri.file_path
+				hostname = cwd_uri.host or wezterm.hostname()
+			else
+				cwd_uri = cwd_uri:sub(8)
+				local slash = cwd_uri:find("/")
+				if slash then
+					hostname = cwd_uri:sub(1, slash - 1)
+					-- Remove the domain name portion of the hostname
+					local dot = hostname:find("[.]")
+					if dot then
+						hostname = hostname:sub(1, dot - 1)
+					end
+					-- and extract the cwd from the uri
+					cwd = cwd_uri:sub(slash)
+					-- table.insert(cells, cwd)
+					if hostname == "" then
+						table.insert(cells, "")
+					elseif string.find(hostname, "arch") then
+						table.insert(cells, "")
+					else
+						table.insert(cells, "")
+					end
 				end
 			end
 		end
